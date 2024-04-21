8586fe11dabb143114488693ea4cb216567ae869
optimusprime <sm4598@columbia.edu>
2024-04-21 10:28:05 -0400

    Added query to analyze rent v bedbugs
    - Fixed the schema to make sure that all zipcodes were consistent dtypes
    - Created a query to find the average Jan rent for top 10 zipcodes with most bedbugs

--- queries/affordable_areas_most_trees.sql
+++ queries/affordable_areas_most_trees.sql
@@ -2,7 +2,7 @@
 SELECT t.zip_code AS zip_code, 
        TO_CHAR(AVG(z."Jan_2024"), 'FM9,999,999.00') AS average_rent
 FROM nyc_trees t
-JOIN zillow_rent_data z ON t.zip_code = CAST(z.zip_code AS text)
+JOIN zillow_rent_data z ON t.zip_code = z.zip_code
 GROUP BY t.zip_code
 ORDER BY COUNT(t.tree_id) DESC
 LIMIT 10;
